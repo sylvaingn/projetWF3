729214ed67baa41e62779221b696e4fa8d5c35f5
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-05-04 15:28:43 +0200

    commentaire

--- updatedProduit.php
+++ updatedProduit.php
@@ -19,7 +19,69 @@ $response->execute([
 
 ]);
 
+
+
+/*
+function checkFile($file)
+{
+    $pathInfoData = pathinfo($file["name"]);
+    $fileExtension = $pathInfoData["extension"];
+
+    if ($fileExtension != "jpg" && $file["type"] != "image/jpeg") {
+        return "Le format n'est pas respecté";
+    }
+    if ($file["size"] > 3145728) {
+        return "Le fichier est trop volumineux";
+    }
+
+    return null;
+}
+
+function saveFile($file)
+{
+    $pathInfoData = pathinfo($file["name"]);
+    $fileName = $pathInfoData["filename"];
+    $fileExtension = $pathInfoData["extension"];
+
+    $nouveauNom = $fileName . uniqid() . "." . $fileExtension;
+    move_uploaded_file($file["tmp_name"], __DIR__ . "/uploads/" . $nouveauNom);
+
+    return $nouveauNom;
+}
+
+function updateProduit($nomFichier)
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=projetAlimentaire;charset=utf8;port=8889', 'root', 'root');
+    $request = "UPDATE varietesProduits 
+                SET nom = :nom , variete = :variete , quantite = :quantite , pays_origine = :pays_origine , photo = :photo
+                WHERE id = :id";
+
+    $response = $bdd->prepare($request);
+    $response->execute([
+
+        "id"            => $_GET["id"],
+        "nom"           => $_POST["nom"],
+        "variete"       => $_POST["variete"],
+        "quantite"      => $_POST["quantite"],
+        "pays_origine"  => $_POST["pays_origine"],
+        "photo"         => $nomFichier
+        ]);
+}
+
+
+if (!empty($_FILES["photo"])){
+    checkFile($_FILES["photo"]);
+    $nom = saveFile($_FILES["photo"]);
+    updateProduit($nom);
+    echo "Réussite";
+}
+
+
+*/
+
+
+
+
+
+
 header("Location: index.php ");
-
-
-?>
