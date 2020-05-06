34bce1ae09d597c484f61a5ed0f151347cc5fa22
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-05-06 12:29:02 +0200

    debug

--- wordpress(test MAJ)/mon_plugin/mon_plugin.php
+++ wordpress(test MAJ)/mon_plugin/mon_plugin.php
@@ -19,6 +19,7 @@ function uninstallHook()
     error_log("plugin désinstallé");
 }
 
-register_activation_hook(__FILE__, activateHook());
-register_deactivation_hook(__FILE__, deactivateHook());
-register_uninstall_hook(__FILE__,uninstallHook());
+
+register_activation_hook(__FILE__, "activateHook" );
+register_deactivation_hook(__FILE__, "deactivateHook");
+register_uninstall_hook(__FILE__,"uninstallHook");
